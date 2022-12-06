f96ba61873c134fab175d0d400a6d9350c1a43ce
buchi1002 <peacek@icloud.com>
2022-12-06 20:57:13 +0900

    reconcsder algorithm

--- SymplecticMatroids.py
+++ SymplecticMatroids.py
@@ -2,76 +2,58 @@ import itertools
 from collections import defaultdict
 import numpy as np
 
-def make_admissible_orderings(N:int = 3):
+def make_admissible_permutations(n:int = 3) -> list:
+    tu_lefts = tuple(itertools.permutations(range(1,n+1)))
+    tu_starreds = tuple(itertools.product((1,-1), repeat=n))
 
-    # J = [n]∪[n]*の作成.
-    Np = list(range(1, N+1))
-    Nm = list(range(-N,0))
+    W = list()
+    for w_left in tu_lefts:
+        for starred in tu_starreds:
+            w = tuple([w_left[-(i+1)]*((-1)*starred[-(i+1)]) for i in range(n)] + [w_left[i]*starred[i] for i in range(n)])
+            W.append(w)
 
-    J = Nm + Np
+    return W
 
-    # 全ての順列について, admissible かどうか調べる.
-    AnsSet = set()
-    for p in itertools.permutations(J):
-        # 順列作成
-        w_dict = defaultdict(lambda:set())
-        for e1 in range(len(p)-1):
-            for e2 in range(e1+1,len(p)):
-                w_dict[p[e1]].add(p[e2])
+def make_admissible_subsets(n:int = 3, k:int = 2) -> list:
+    li_nums = tuple(range(1,n+1))
+    li_starreds = tuple(itertools.product((1,-1), repeat=k))
 
-        # admissible 判定
-        flag = True
-        for e1 in range(len(p)-1):
-            for e2 in range(e1+1,len(p)):
-                if ((-p[e1]) in w_dict[-p[e2]]) == False:
-                    flag = False
+    Jk = list()
+    for nums in itertools.combinations(li_nums, k):
+        for starreds in li_starreds:
+            K = tuple(nums[i]*starreds[i] for i in range(k))
 
-        if flag:
-            AnsSet.add(p)
+            Jk.append(K)
 
-    for Ans in AnsSet:
-        print("---")
-        print(tuple(J))
-        print(Ans)
+    return Jk
 
-    print("---")
-    print(len(AnsSet))
+def make_admissible_orderings(w:tuple) -> set:
+    admissible_orderings = set()
+    for es in range(len(w)):
+        for eb in range(es,len(w)):
+            admissible_orderings.add(es,eb)
 
-def SearchSymmetrycABt(N:int = 3) -> list:
-    L = list(range(N))
-    all = list(itertools.combinations_with_replacement(L,N)
-)
-    matrices = []
-    for row1 in all:
-        for row2 in all:
-            for row3 in all:
-                arr = np.array([row1, row2, row3])
-                matrices.append(arr)
+    return admissible_orderings
+
+
+def make_isotropic_subspace(p:int = 3, n:int = 3, k:int = 4) -> list:
+    F = list(range(p))
+    all_row = itertools.combinations_with_replacement(F,n)
+    all_row = list(map(list, all_row))
+
+    matrices = list(itertools.combinations_with_replacement(all_row,k))
+    matrices = list(map(lambda x:np.array(list(x)), matrices))
     matrices = matrices[1:]
 
-    founds = list()
-    length = len(matrices)
-    O = np.array([[0,0,0] for _ in range(N)])
-    for idx_A in range(length-1):
-        for idx_B in range(idx_A+1,length):
+    isotropic_subsspace = list()
+    for idx_A in range(len(matrices)-1):
+        for idx_B in range(idx_A + 1,len(matrices)):
             A = matrices[idx_A]
             B = matrices[idx_B]
 
-            ABt = np.dot(A,B.T)%3
+            ABt = np.dot(A, B.T)%p
             BtA = ABt.T
-            if (np.array_equal(ABt, O) == False) and np.array_equal(ABt, BtA):
-                if np.linalg.matrix_rank(np.hstack((A, B)), tol=None) == 3:
-                    founds.append([A, B, ABt])
+            if np.array_equal(ABt, BtA) and np.linalg.matrix_rank(np.hstack((A, B)), tol=None) == k:
+                isotropic_subsspace.append([A, B, ABt])
 
-    for A, B, ABt in founds:
-        print("A")
-        print(A)
-        print("B")
-        print(B)
-        print("ABt")
-        print(ABt)
-
-    print(len(founds))
-
-makep()
-print("end")
+    return isotropic_subsspace
