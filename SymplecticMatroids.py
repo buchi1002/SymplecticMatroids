ce71a6344f5e49a154bdd5adb0d4d63452f73f83
buchi1002 <peacek@icloud.com>
2022-12-10 01:33:08 +0900

    reconsider the algorythm and settings of functon

--- SymplecticMatroids.py
+++ SymplecticMatroids.py
@@ -1,11 +1,12 @@
 import itertools
 from collections import defaultdict
 import numpy as np
+import math
 
-def make_base(n:int = 3) -> list:
+def make_base(n:int) -> list:
     return list(range(-n,0)) + list(range(1,n+1))
 
-def make_admissible_permutations(n:int = 3) -> list:
+def make_admissible_permutations(n:int) -> list:
     tu_lefts = tuple(itertools.permutations(range(1,n+1)))
     tu_starreds = tuple(itertools.product((1,-1), repeat=n))
 
@@ -17,7 +18,7 @@ def make_admissible_permutations(n:int = 3) -> list:
 
     return W
 
-def make_admissible_subsets(n:int = 3, k:int = 2) -> list:
+def make_admissible_subsets(n:int, k:int) -> list:
     li_nums = tuple(range(1,n+1))
     li_starreds = tuple(itertools.product((1,-1), repeat=k))
 
@@ -40,20 +41,35 @@ def make_admissible_ordering(w:tuple) -> dict:
     return w_ordering
 
 
-def make_isotropic_subspace(p:int = 3, n:int = 3, k:int = 4) -> list:
-    F = list(range(p))
-    all_row = itertools.combinations_with_replacement(F,n)
+def make_isotropic_subspace(q:int, n:int, k:int, limit:int = -1, start:int = 0) -> list:
+
+    all_row = itertools.product(range(q),repeat = n)
     all_row = tuple(map(lambda x:np.array(x), all_row))
 
     matrices = list(itertools.combinations_with_replacement(all_row,k))
     matrices = list(map(lambda x:np.array(x), matrices))
-    matrices = matrices
+
+    matrices = matrices[start:]
 
     isotropic_subsspace = list()
-    for AB in itertools.combinations(matrices, 2):
-        A, B = AB
-        ABt = np.dot(A, B.T)%p
-        if np.array_equal(ABt, ABt.T):
-            isotropic_subsspace.append((A, B))
+    if limit == -1:
+        for AB in itertools.combinations(matrices, 2):
+            A, B = AB
+            ABt = (A@B.T)%q
+            if np.array_equal(ABt, ABt.T):
+                isotropic_subsspace.append((A, B))
+
+    else:
+        count = 0
+        for AB in itertools.combinations(matrices, 2):
+            A, B = AB
+            ABt = (A@B.T)%q
+            if np.array_equal(ABt, ABt.T):
+                isotropic_subsspace.append((A, B))
+                count += 1
+
+                if count >= limit:
+
+                    return isotropic_subsspace
 
     return isotropic_subsspace
